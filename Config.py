d1ff752e2a4287ccf78ac9e19548fe3f3e048f64
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-02-28 10:26:17 -0400

    Update Config.py

--- Config.py
+++ Config.py
@@ -8,15 +8,15 @@ class Config():
     APP_ID = os.environ.get("APP_ID", 6)
     API_HASH = os.environ.get("API_HASH", None)
     SUDO_USERS = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
-    SUDO_USERS.append(939425014)
+    SUDO_USERS.append(1192065197)
     SUDO_USERS = list(set(SUDO_USERS))
   else:
-    BOT_TOKEN = ""
+    BOT_TOKEN = "1622814733:AAEyxcB5n6FwzBVIT2LgO-oyH2iqWwOgO2M"
     DATABASE_URL = ""
-    APP_ID = ""
-    API_HASH = ""
+    APP_ID = "2788465"
+    API_HASH = "fc41e33d78927150ad3f81c257f7cf31"
     SUDO_USERS = list(set(int(x) for x in ''.split()))
-    SUDO_USERS.append(939425014)
+    SUDO_USERS.append(1192065197)
     SUDO_USERS = list(set(SUDO_USERS))
 
 
